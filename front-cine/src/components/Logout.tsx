e3c223377796d183b4bf3b31211d436895afbf64
BeehnoodLab <behnood.pazhang@yahoo.com>
2025-08-05 14:01:36 +0200

    develop/bhn-dev : button Logout est installé.

---
+++ front-cine/src/components/Logout.tsx
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { useAuth } from "../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+const Logout = () => {
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    logout(); 
+    navigate("/"); // redirection
+  }, []);
+
+  return null;
+};
+
+export default Logout;
